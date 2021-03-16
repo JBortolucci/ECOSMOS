d850bd9762b8755d308a1b98b2f8b71c9f0022b9
svcuadra <santiago.cuadra@embrapa.br>
2021-03-16 18:44:33 -0300

    inisoil

--- R/Initial/inisoil.R
+++ R/Initial/inisoil.R
@@ -132,7 +132,7 @@ inisoil <- function() {
     if(!is.na(SOIL.profile$SDUL[k])) {sfield[k]   <- (1 / poros[k]) * SOIL.profile$SDUL[k] } # field capacity as fraction of porosity (volume fraction)
     if(!is.na(SOIL.profile$SLLL[k])) {swilt[k]    <- (1 / poros[k]) * SOIL.profile$SLLL[k] } # wilting point as fraction of porosity  (volume fraction)
     if(!is.na(SOIL.profile$SSKS[k])) {hydraul[k]  <- SOIL.profile$SSKS[k] / (1000 * 3600)  } # SHC : saturated hydraulic conductivity (m s-1)
-    #TODO comentei aqui Santiago, acho q passou isso... if(!is.na(SOIL.profile$AEP[k]))  {suction[k]  <- 1.261*(SOIL.profile$SLLL[k]) + 0.027  } # AEP : air entry potential (m-H20)
+    if(!is.na(SOIL.profile$SLLL[k]))  {suction[k]  <- 1.261*(SOIL.profile$SLLL[k]) + 0.027  } # AEP : air entry potential (m-H20)
     if(!is.na(SOIL.profile$AEP[k]))  {suction[k]  <- SOIL.profile$AEP[k]                   } # AEP : air entry potential (m-H20)
     if(!is.na(SOIL.profile$BEXP[k])) {bex[k]      <- SOIL.profile$BEXP[k]                  } # Campbell's 'b' exponent
     if(!is.na(SOIL.profile$SRGF[k])) {SRGF[k]     <- SOIL.profile$SRGF[k]                  } # Root hospitality factor 
